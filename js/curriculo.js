c77b6886bd0dd481c0ece0fcb18702e1a0bcde22
Vinícius Ferreira Salge <104887873+salgezinho@users.noreply.github.com>
2023-06-26 18:39:29 -0300

    Add files via upload

---
+++ js/curriculo.js
@@ -0,0 +1,29 @@
+var banners = document.querySelectorAll(".banner");
+var dots = document.querySelectorAll(".dot");
+var currentIndex = 0;
+
+function showBanner(index) {
+  for (var i = 0; i < banners.length; i++) {
+    banners[i].classList.remove("active");
+    dots[i].classList.remove("active");
+  }
+
+  banners[index].classList.add("active");
+  dots[index].classList.add("active");
+}
+
+function nextBanner() {
+  currentIndex++;
+  if (currentIndex === banners.length) {
+    currentIndex = 0;
+  }
+  showBanner(currentIndex);
+}
+
+setInterval(nextBanner, 3000); // Troca de banner a cada 3 segundos (3000ms)
+
+const form = document.querySelector("form");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  alert("Curriculum enviado com sucesso!");
+});
